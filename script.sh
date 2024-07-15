4886819605cdedbbd4096395dc797d2baf708b15
NotaNerd021 <124067502+NotaNerd021@users.noreply.github.com>
2024-07-15 15:24:10 +0330

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,4 @@
+cd /var/www/html/wizwizxui-timebot/ && rm config.php bot.php &&
+wget https://github.com/NotaNerd021/botphp-s/raw/main/bot.php && wget https://github.com/NotaNerd021/botphp-s/raw/main/config.php
+cd /var/www/html/wizwizxui-timebot/settings/ && rm values.php &&
+wget https://github.com/NotaNerd021/botphp-s/raw/main/values.php
