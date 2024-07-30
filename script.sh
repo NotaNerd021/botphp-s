948cfa9aa5a47ce07d3626271279c425fa5e6eb7
NotaNerd021 <124067502+NotaNerd021@users.noreply.github.com>
2024-07-30 05:03:49 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-cd /var/www/html/wizwizxui-timebot/ && rm config.php bot.php &&
-wget https://github.com/NotaNerd021/botphp-s/raw/main/bot.php && wget https://github.com/NotaNerd021/botphp-s/raw/main/config.php
+cd /var/www/html/wizwizxui-timebot/ && rm bot.php &&
+wget https://github.com/NotaNerd021/botphp-s/raw/main/bot.php
 cd /var/www/html/wizwizxui-timebot/settings/ && rm values.php &&
 wget https://github.com/NotaNerd021/botphp-s/raw/main/values.php
